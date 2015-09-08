f4b6cc1f387d34020ebfb98660abdd69f3c3cf2c
faviouz <fabiu.maia@gmail.com>
2015-09-08 16:38:44 +0100

    cleanup

--- api/helpers.py
+++ api/helpers.py
@@ -2,17 +2,22 @@ import hashlib
 import os
 import json
 
+
 def md5(string):
     return hashlib.md5(str(string)).hexdigest()
+
 
 def list_to_csv(a):
     return ','.join(map(str, list(a)))
 
+
 def remove_string_whitespace(string):
     return string.replace(' ', '')
 
+
 def abspath_from_relativepath(relative_path):
     return os.path.abspath(os.path.join(os.path.dirname(__file__), relative_path))
+
 
 def json_from_file(filename):
     return json.load(open(filename, 'r'))
