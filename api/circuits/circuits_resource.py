a12eedc1e92d8d5059a0479d7e8ad84a7325cd85
faviouz <fabiu.maia@gmail.com>
2015-09-08 15:06:32 +0100

    filter and simplify endpoint

--- api/circuits/circuits_resource.py
+++ api/circuits/circuits_resource.py
@@ -1,14 +1,23 @@
 import json
 from api.circuits.circuits_foursquare import CircuitsFoursquare
-
+from api.circuits.util.PlacesList import PlacesList
 
 class CircuitsResource:
-    def on_get(self, request, response, location = 'Porto', number_of_places = 5, interests = []):
+    def on_get(self, request, response, location = 'Porto', number_of_places = 5, interests = ['coffee']):
         circuits_foursquare = CircuitsFoursquare()
 
         results = circuits_foursquare.search(location, number_of_places, interests)
 
+        result = PlacesList()
+
+        for venue in results['venues']:
+            result.extend([{
+                'location': venue['location'],
+                'name': venue['name']
+            }])
+
+
         # TODO: pass the list of places through the algorithm to generate a circuit
-        circuits = results
+        circuits = result
 
         response.body = json.dumps(circuits)
