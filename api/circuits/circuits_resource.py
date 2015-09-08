c5dc4c38d7ecfd49bf2c02131d00cdfb33f906ee
faviouz <fabiu.maia@gmail.com>
2015-09-08 20:33:58 +0100

    fix resource

--- api/circuits/circuits_resource.py
+++ api/circuits/circuits_resource.py
@@ -4,7 +4,7 @@ from api.places.places_list import PlacesList
 
 
 class CircuitsResource:
-    def on_get(self, request, response, location='Porto', number_of_places=5, interests=['coffee']):
+    def on_get(self, request, response, location, number_of_places=5, interests=['coffee']):
         foursquare_response = FoursquareClient().search(location, number_of_places, interests)
 
         places = PlacesList()
@@ -15,6 +15,12 @@ class CircuitsResource:
             }])
 
         # TODO: pass the list of places through the algorithm to generate a circuit
-        # places.sort()
+        # TODO: get current location
+        places.order({
+            'location': {
+                'lat': 41.14870842690923,
+                'lng': -8.61085212647548
+            }
+        })
 
         response.body = json.dumps(places, separators=(',', ':'))
