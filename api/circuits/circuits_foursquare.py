bd70b0bfcad91d54dece7ecb88070df6fb84d89b
faviouz <fabiu.maia@gmail.com>
2015-09-08 01:13:13 +0100

    wip foursquare api implementation

---
+++ api/circuits/circuits_foursquare.py
@@ -0,0 +1,20 @@
+import json
+import os
+from api.helpers import *
+import foursquare
+
+class CircuitsFoursquare:
+    def __init__(self):
+        config = json_from_file(os.path.join(os.path.dirname(__file__), '../config/foursquare.json'))
+
+        self.client = foursquare.Foursquare(
+            client_id = config['client_id'],
+            client_secret = config['client_secret']
+        )
+
+    def search(self, location, number_of_places = 5, interests = []):
+        return self.client.venues.search(params = {
+            'near': location,
+            'limit': number_of_places,
+            'query': list_to_csv(interests)
+        })
--- api/circuits/circuits_resource.py
+++ api/circuits/circuits_resource.py
@@ -1,13 +1,13 @@
 import json
+from api.circuits.circuits_foursquare import CircuitsFoursquare
 
 class CircuitsResource:
-    def __init__(self):
-        pass
+    def on_get(self, request, response, location = 'Porto', number_of_places = 5, interests = []):
+        circuits_foursquare = CircuitsFoursquare()
 
-    def on_get(self, request, response):
-        circuit = {
-            'foo': 5,
-            'bar': 7
-        }
+        results = circuits_foursquare.search(location, number_of_places, interests)
 
-        response.body = json.dumps(circuit)
+        # TODO: pass the list of places through the algorithm to generate a circuit
+        circuits = results
+
+        response.body = json.dumps(circuits)
