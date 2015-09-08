c531708cf1dac937a97e151586289580548eead0
Ricardo Jesus <rj.bcjesus@gmail.com>
2015-09-08 16:38:15 +0100

    Test algorithm #1

--- api/places/places.py
+++ api/places/places.py
@@ -3,4 +3,6 @@ from api.clients.googlemaps_client import GoogleMapsClient
 
 class Place(dict):
     def distance_to(self, p):
-        return GoogleMapsClient().distance_between(self, p)
+        lat1, lng1 = self['location']['lat'], self['location']['lng']
+        lat2, lng2 = p['location']['lat'], p['location']['lng']
+        return GoogleMapsClient().distance_between((lat1, lng1), (lat2, lng2))
