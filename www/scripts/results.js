56331cc7ae8b1e5783959ce67988aab72e77aa84
faviouz <fabiu.maia@gmail.com>
2015-09-09 22:51:40 +0100

    wip map drawing and geolocation

--- www/scripts/results.js
+++ www/scripts/results.js
@@ -1,47 +1,75 @@
 var map;
-var currentPosition;
+var currentLocation;
 
 GMaps.geolocate({
-    success: function(position) {
-        latitude = position.coords.latitude;
-        longitude = position.coords.longitude;
+    success: function(location) {
+        latitude = location.coords.latitude;
+        longitude = location.coords.longitude;
 
-        currentPosition = {
-            'location': {
-                'lat': latitude,
-                'lng': longitude
-            }
+        currentLocation = {
+            'lat': latitude,
+            'lng': longitude
         };
 
-        console.log(currentPosition);
+        console.log(currentLocation);
 
         map = new GMaps({
             el: '#map',
             'lat': latitude,
             'lng': longitude
         });
+
+        map.addMarker({
+            lat: latitude,
+            lng: longitude,
+            title: "XPTO",
+            click: function(e) {
+                alert('You clicked in this marker');
+            }
+        });
+
+        map.setCenter(latitude, longitude);
+
+        $.get('http://192.168.43.215:1337/circuits/' + currentLocation['lat'] + ',' + currentLocation['lng'] + '/coffee/5/', function(places) {
+            console.log(places);
+
+            places.forEach(function(place) {
+                map.addMarker({
+                    lat: place.location.lat,
+                    lng: place.location.lng,
+                    title: place.name,
+                    click: function(e) {
+                        alert('You clicked in this marker');
+                    }
+                });
+            });
+
+            for (var i = 1; i < places.length; i++) {
+                origin = [places[i-1]['location']['lat'], places[i-1]['location']['lng']]
+                destination = [places[i]['location']['lat'], places[i]['location']['lng']]
+
+                console.log(origin);
+                console.log(destination);
+
+                map.drawRoute({
+                    origin: origin,
+                    destination: destination,
+                    travelMode: 'walking',
+                    strokeColor: '#ff0000',
+                    strokeOpacity: 0.6,
+                    strokeWeight: 6
+                });
+            };
+        });
+
     },
     error: function(error) {
         alert('Geolocation failed: '+error.message);
     },
     not_supported: function() {
-        alert("Your browser does not support geolocation");
+        alert('Your browser does not support geolocation');
     },
     always: function() {
-        alert("Done!");
+        alert('Done!');
     }
-});
-
-$.get("http://127.0.0.1:1337/circuits/43.343434,43.43434343/5/coffee", function(places) {
-    places.forEach(function(place) {
-        map.addMarker({
-            lat: place.location.lat,
-            lng: place.location.lng,
-            title: place.name,
-            click: function(e) {
-                alert('You clicked in this marker');
-            }
-        });
-    });
-
 });
