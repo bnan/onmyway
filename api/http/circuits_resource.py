22b0136dcbdd0512c984e557740ca87b22287c63
Ricardo Jesus <rj.bcjesus@gmail.com>
2015-09-12 20:24:44 +0100

    API validation

--- api/http/circuits_resource.py
+++ api/http/circuits_resource.py
@@ -8,7 +8,8 @@ maps_to_generate = 3
 
 
 class CircuitsResource:
-    def on_get(self, request, response, current_location, interests, number_of_places=5):
+    @staticmethod
+    def on_get(request, response, current_location, interests, number_of_places=5):
         number_of_places = int(number_of_places)
         venues = FoursquareClient().search(current_location, number_of_places * maps_to_generate, interests)['venues']
 
@@ -31,14 +32,16 @@ class CircuitsResource:
             places[i].tripify(current_location)
 
             i += 1
+
         response.body = json.dumps([place.__dict__() for place in places], separators=(',', ':'))
         response.set_header('Access-Control-Allow-Origin', '*')
 
 
 def snap_to_roads(places):
     lat_lng = [(place.get_lat(), place.get_lng()) for place in places]
-    results = GoogleMapsClient().client.snap_to_roads(lat_lng)
-    for result in results:
-        place = places[result['originalIndex']]
-        place.set_lat(result['location']['latitude'])
-        place.set_lng(result['location']['longitude'])
+    if lat_lng:
+        results = GoogleMapsClient().client.snap_to_roads(lat_lng)
+        for result in results:
+            place = places[result['originalIndex']]
+            place.set_lat(result['location']['latitude'])
+            place.set_lng(result['location']['longitude'])
