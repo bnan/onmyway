3fbf3ea6e88c84470df0080b3ae42a2226a93200
faviouz <fabiu.maia@gmail.com>
2015-09-13 05:06:10 +0100

    change icon size

--- www/scripts/index.js
+++ www/scripts/index.js
@@ -41,6 +41,8 @@ $(document).ready(function() {
         };
     });
 
+
+
     $('#results-icon').on('click', function() {
         remove(['currentLocation', 'routes']);
         switchPageView();
